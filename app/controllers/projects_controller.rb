22bef3aa39c1f88612935f8e7ab0d6be302353d6
Bhekisile <bhekisilejozi@gmail.com>
2026-08-16 05:52:21 +0200

    Generate projectsController, modified route.rb

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,56 @@
+class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @projects = Project.all
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to projects_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @project.update(project_params)
+      redirect_to @project,
+                  notice: "Project updated successfully."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @project.destroy
+
+    redirect_to projects_path,
+                notice: "Project deleted successfully."
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(
+      :name,
+      :description
+    )
+  end
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
+end
