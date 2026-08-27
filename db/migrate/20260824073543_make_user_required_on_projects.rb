d199de71d15f86fc7c2b5dff1037957b685bbe07
Bhekisile <bhekisilejozi@gmail.com>
2026-08-27 13:10:13 +0200

    Add index.html for the dashboard and run migration

---
+++ db/migrate/20260824073543_make_user_required_on_projects.rb
@@ -0,0 +1,5 @@
+class MakeUserRequiredOnProjects < ActiveRecord::Migration[8.1]
+  def change
+    change_column_null :projects, :user_id, false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_22_181006) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_24_073543) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -20,7 +20,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_22_181006) do
     t.string "name"
     t.integer "status", default: 0, null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
