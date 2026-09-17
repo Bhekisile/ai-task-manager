c7ef8c8e6b2c1c9248df6b99d0925810fcecd3a4
Bhekisile <bhekisilejozi@gmail.com>
2026-09-17 15:30:43 +0200

    Implement Project Policy show, create, edit, and update.

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -13,11 +13,11 @@ class ApplicationPolicy
   end
 
   def show?
-    false
+    user.admin? || record.user == user
   end
 
   def create?
-    false
+    true
   end
 
   def new?
@@ -25,7 +25,7 @@ class ApplicationPolicy
   end
 
   def update?
-    false
+    user.admin? || record.user == user
   end
 
   def edit?
