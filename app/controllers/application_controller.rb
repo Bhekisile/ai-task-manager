fe97bf1cdb205a5f1e5802f1a67056e59a10f45e
Bhekisile <bhekisilejozi@gmail.com>
2026-09-17 15:38:15 +0200

    Handle errors with rescue_from for Pundit NotAuthorisedError and create user_not_authorized method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,19 @@ class ApplicationController < ActionController::Base
   
   before_action :authenticate_user!
   
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  private
+
+  def user_not_authorized
+    redirect_to projects_path,
+                alert: "You do not have permission to perform this action."
+  end
 end
